434d78b9389b43a32a437f11bf80bedcca00b0d0
WisecarverWorks <wisecarvertruth@gmail.com>
2022-10-23 06:39:22 -0500

    docs: modified artwork_application_backend urls.py file, admin.py and urls.py

---
+++ shop_dashboard/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path
+
+from .middlewares.auth import auth_middleware
+from .views.cart import Cart
+from .views.checkout import CheckOut
+from .views.home import Index, shop_dashboard
+from .views.login import Login, logout
+from .views.orders import OrderView
+from .views.signup import Signup
+
+urlpatterns = [
+    path('', Index.as_view(), name='homepage'),
+    path('shop_dashboard', shop_dashboard , name='shop_dashboard'),
+
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout , name='logout'),
+    path('cart', auth_middleware(Cart.as_view()) , name='cart'),
+    path('check-out', CheckOut.as_view() , name='checkout'),
+    path('orders', auth_middleware(OrderView.as_view()), name='orders'),
+
+]
